6e0f9abf5479c79602240cf135b5f51c62d0155d
caio-cris <caiohcris@gmail.com>
2022-10-30 11:01:58 -0300

    Criando a entidade category com TDD

--- application/src/main/java/com/fullcycle/admin/catalogo/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/UseCase.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo;
 
+import com.fullcycle.admin.catalogo.domian.category.Category;
+
 public class UseCase {
     public Category execute() {
         return new Category();
