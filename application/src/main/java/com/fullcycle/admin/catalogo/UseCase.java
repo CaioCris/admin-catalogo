669f6529c9e154b7c5801e5a2ad9be608430b255
caio-cris <caiohcris@gmail.com>
2022-10-30 23:23:56 -0300

    Revert "Criando a entidade category com TDD"
    This reverts commit 6e0f9abf5479c79602240cf135b5f51c62d0155d.

--- application/src/main/java/com/fullcycle/admin/catalogo/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/UseCase.java
@@ -1,7 +1,5 @@
 package com.fullcycle.admin.catalogo;
 
-import com.fullcycle.admin.catalogo.domian.category.Category;
-
 public class UseCase {
     public Category execute() {
         return new Category();
