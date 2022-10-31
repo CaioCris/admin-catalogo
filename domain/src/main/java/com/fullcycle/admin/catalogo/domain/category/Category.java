bbca454d66140f6566edeea91a80f44765771213
caio-cris <caiohcris@gmail.com>
2022-10-31 00:08:55 -0300

    Tornando a classe category um AggregateRoot

--- domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/category/Category.java
@@ -1,5 +1,65 @@
 package com.fullcycle.admin.catalogo.domain.category;
 
-public class Category {
-    public String id;
+import com.fullcycle.admin.catalogo.domain.AggregateRoot;
+
+import java.time.Instant;
+
+public class Category extends AggregateRoot<CategoryID> {
+
+    private String name;
+    private String description;
+    private boolean active;
+    private Instant createdAt;
+    private Instant updatedAt;
+    private Instant deletedAt;
+
+    public Category(final CategoryID categoryID,
+                    final String name,
+                    final String description,
+                    final boolean active,
+                    final Instant createdAt,
+                    final Instant updatedAt,
+                    final Instant deletedAt) {
+        super(categoryID);
+        this.name = name;
+        this.description = description;
+        this.active = active;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+    }
+
+    public static Category newCategory(final String name, final String description, final boolean active) {
+        final var id = CategoryID.unique();
+        final var now = Instant.now();
+        return new Category(id, name, description, active, now, now, null);
+    }
+
+    public CategoryID getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
 }
