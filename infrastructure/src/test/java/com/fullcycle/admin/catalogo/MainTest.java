7d811023c4c5c34bc35487637b18f3f46751feb8
caio-cris <caiohcris@gmail.com>
2022-11-10 15:51:21 -0300

    Configurando o datasource de testes da aplicação

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/MainTest.java
@@ -6,9 +6,11 @@ package com.fullcycle.admin.catalogo;
 import com.fullcycle.admin.catalogo.infrastructure.Main;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
 
 class MainTest {
     @Test void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
