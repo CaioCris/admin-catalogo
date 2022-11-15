30900aeb60d6496c54d880e1146c663ff6e3b732
caio-cris <caiohcris@gmail.com>
2022-11-15 17:20:12 -0300

    Testes integrados do category repository para cenários not nullable

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.fullcycle.admin.catalogo.infrastructure.category.persistence;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.MySQLGatewayTest;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAnInvalidNullName_whenCallsSave_shouldReturnError() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedPropertyName = "name";
+        final var expectedPropertyMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.%s".formatted(expectedPropertyName);
+
+        final var category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var entity = CategoryJpaEntity.from(category);
+        entity.setName(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(entity));
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedPropertyMessage, actualCause.getMessage());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullCreatedAt_whenCallsSave_shouldReturnError() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedPropertyName = "createdAt";
+        final var expectedPropertyMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.%s".formatted(expectedPropertyName);
+
+        final var category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var entity = CategoryJpaEntity.from(category);
+        entity.setCreatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(entity));
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedPropertyMessage, actualCause.getMessage());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullUpdatedAt_whenCallsSave_shouldReturnError() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedPropertyName = "updatedAt";
+        final var expectedPropertyMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity.%s".formatted(expectedPropertyName);
+
+        final var category = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var entity = CategoryJpaEntity.from(category);
+        entity.setUpdatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(entity));
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals(expectedPropertyName, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedPropertyMessage, actualCause.getMessage());
+
+    }
+
+}
