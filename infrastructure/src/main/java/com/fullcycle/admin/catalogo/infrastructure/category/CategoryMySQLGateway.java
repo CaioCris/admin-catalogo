44c1313a7f0772c7043b57e363206270388b8e94
caio-cris <caiohcris@gmail.com>
2022-11-15 16:59:18 -0300

    Implementando o método create do gateway de categoria

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -5,6 +5,7 @@ import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
 import com.fullcycle.admin.catalogo.domain.category.CategoryID;
 import com.fullcycle.admin.catalogo.domain.category.CategorySearchQuery;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +22,8 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
 
     @Override
-    public Category create(Category category) {
-        return null;
+    public Category create(final Category category) {
+        return this.repository.save(CategoryJpaEntity.from(category)).toAggregate();
     }
 
     @Override
